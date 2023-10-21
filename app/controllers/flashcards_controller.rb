1fc1ebfb96f12638c892054dd2705069eae032ae
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-10-21 16:37:48 +0200

    ui fix. next and prev converted from button to div with correct routes

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -1,5 +1,15 @@
 class FlashcardsController < ApplicationController
 
-  def index
+  before_action :set_flashcard, only: [:show]
+
+  def show
+    @prev_id = @flashcard.id - 1 unless @flashcard.id == 1
+    @next_id = @flashcard.id + 1 unless @flashcard.id == Flashcard.last.id
   end
+
+  private
+
+  def set_flashcard
+    @flashcard = Flashcard.find_by(id: params[:id]) || Flashcard.first
+  end 
 end
