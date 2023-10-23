3c4856cc98017e4a830b9721cd92f500b980463b
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-10-23 16:49:13 +0200

    bug with flashcards controller

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -10,9 +10,35 @@ class FlashcardsController < ApplicationController
     @next_id = @all_ids[current_position + 1] if current_position && current_position < @all_ids.length - 1
   end
 
+  def mark_correct  
+    update_statistic(true)
+    render partial: 'flashcards/flashcard', locals: { flashcard: @flashcard }
+
+  end 
+    
+  def mark_incorrect 
+    update_statistic(false)
+    render partial: 'flashcards/flashcard', locals: { flashcard: @flashcard }
+
+  end  
+ 
+
   private
 
   def set_flashcard
     @flashcard = Flashcard.find_by(id: params[:id]) || Flashcard.first
+  end 
+
+  def update_statistic(is_correct) 
+    flashcard = Flashcard.find(params[:id])
+    statistic = FlashcardStatistic.find_or_initialize_by(user: current_user, flashcard: flashcard)
+  
+    if is_correct 
+      statistic.correct_count += 1
+    else 
+      statistic.incorrect_count += 1
+    end 
+  
+    statistic.save 
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :flashcards
+
+  resources :flashcards do
+    member do
+      post :mark_correct
+      post :mark_incorrect
+    end
+  end
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
