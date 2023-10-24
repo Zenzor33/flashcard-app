06440271a532eafd4ff3ec10995473fd81a0fbb2
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-10-24 09:23:36 +0200

    annotated models successfully

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: flashcards
+#
+#  id         :integer          not null, primary key
+#  back       :string
+#  front      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Flashcard < ApplicationRecord
   has_many :flashcard_statistics
   has_many :users, through: :flashcard_statistics
--- app/models/flashcard_statistic.rb
+++ app/models/flashcard_statistic.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: flashcard_statistics
+#
+#  id              :integer          not null, primary key
+#  correct_count   :integer          default(0)
+#  incorrect_count :integer          default(0)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  flashcard_id    :integer          not null
+#  user_id         :integer          not null
+#
+# Indexes
+#
+#  index_flashcard_statistics_on_flashcard_id  (flashcard_id)
+#  index_flashcard_statistics_on_user_id       (user_id)
+#
+# Foreign Keys
+#
+#  flashcard_id  (flashcard_id => flashcards.id)
+#  user_id       (user_id => users.id)
+#
 class FlashcardStatistic < ApplicationRecord
   belongs_to :user 
   belongs_to :flashcard  
