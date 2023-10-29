038d981862903a010e8bd6bd1b9d6f1a6a699711
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-10-29 06:35:05 +0100

    added debounce

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,10 +1,22 @@
 import { Controller } from "@hotwired/stimulus";
 
+function debounce(func, wait) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      timeout = null;
+      func.apply(context, args);
+    }, wait);
+  };
+}
+
 // Connects to data-controller="form"
 export default class extends Controller {
   connect() {}
 
-  submit() {
+  submit = debounce(function () {
     this.element.requestSubmit();
-  }
+  }, 500);
 }
