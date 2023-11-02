157babf5e52d0c87d7b0c48e3d394ba25138c060
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-11-02 09:13:28 +0100

    updated navbar - new_user_session_path in my-cards

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,4 +1,22 @@
 class DecksController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index 
+    # Redirect to show with choice of category by default
+    # Show path: /user/:user_id/decks/:id(.:format)
+    # The last :id will represent the category (All, New, Learning, Progressing, Mastered)
+    # The default is all.
+    # If user's deck is empty, render an empty page
+
+    # if deck.exist
+      # redirect_to action:show id - getIdfFromMethodIn Deck.rb
+    # else
+      #render :empty
+    #end
+    
+  end 
+
+  def show 
   end 
 end
