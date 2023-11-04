049ccbc00aa2922879005140e4418188cd297e89
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-11-04 16:40:31 +0100

    updated navbar conditional syling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def current_or_default(path)
+    current = 'bg-gray-900 text-white'
+    default = "text-gray-300 hover:bg-gray-700 hover:text-white" 
+    current_page?(path) ? current : default
+  end 
 end
