97980a8bb225c0502b69de55d00a1437a118f30c
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-11-06 06:37:05 +0100

    fixed styles

--- app/helpers/decks_helper.rb
+++ app/helpers/decks_helper.rb
@@ -1,5 +1,5 @@
 module DecksHelper
   def user_deck_classes(category)
-    "rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 #{'bg-green-500 text-white hover:text-black hover:bg-green-500' if params[:category] == category}" 
+    "rounded-full px-4 py-2.5 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 #{'bg-white text-gray-900 hover:bg-gray-50' if params[:category] != category} #{'bg-green-500 text-white hover:text-black hover:bg-green-500' if params[:category] == category}" 
   end
 end
