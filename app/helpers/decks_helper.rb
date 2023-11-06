3f774fc9c3a453738af3a078f3aef37a0b35501d
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-11-06 06:33:07 +0100

    added stylings to decks show category buttons

--- app/helpers/decks_helper.rb
+++ app/helpers/decks_helper.rb
@@ -1,2 +1,5 @@
 module DecksHelper
+  def user_deck_classes(category)
+    "rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 #{'bg-green-500 text-white hover:text-black hover:bg-green-500' if params[:category] == category}" 
+  end
 end
