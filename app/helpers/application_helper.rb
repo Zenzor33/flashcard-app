744ac9a2a0b522a783232dc79d363b82f3ad112b
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-11-09 17:22:50 +0100

    refactored study link navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     default = "text-gray-300 hover:bg-gray-700 hover:text-white" 
     current_page?(path) ? current : default
   end 
+
+  def navbar_link_class(path)
+    "#{current_or_default(path)} rounded-md px-3 py-2 text-sm font-medium"
+  end 
 end
