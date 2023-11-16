5f88aa07a19265955588afec682b22131e1be548
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-11-16 10:44:42 +0100

    next and prev hrefs fully functional

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -40,17 +40,9 @@ class StudiesController < ApplicationController
   
   def set_deck_and_deck_flashcards
     @deck = Deck.find_by(user_id: params[:user_id])
-    puts "DECK BELOW"
-    puts @deck
     @deck_flashcards = @deck.deck_flashcards.order(id: :asc)
-    puts "DECK_FLASHCARDS BELOW"
-    puts @deck_flashcards
     @deck_flashcard = @deck_flashcards.find_by(id: params[:deck_flashcard]) || @deck_flashcards.find_by(id: params[:format])
-    puts "DECK_FLASHCARD BELOW"
-    puts @deck_flashcard
     @flashcard = Flashcard.find(@deck_flashcard.flashcard_id) 
-    puts "FLASHCARD BELOW"
-    puts @flashcard 
   end 
 
   def set_flashcard_and_flashcard_statistics 
@@ -59,17 +51,9 @@ class StudiesController < ApplicationController
 
   def set_next_and_previous_flashcards
     next_deck_flashcards = @deck_flashcards.where('id > ?', @deck_flashcard.id)
-    if next_deck_flashcards.empty?
-      @next_deck_flashcard = nil
-    else  
-      next_deck_flashcard = next_deck_flashcards.first
+    next_deck_flashcards.empty? ? @next_deck_flashcard = nil : @next_deck_flashcard = next_deck_flashcards.first
 
-      next_flashcard = Flashcard.find_by(id: next_deck_flashcard.flashcard_id) || Flashcard.find_by(id: @deck_flashcards.last.flashcard_id)
-      @next_deck_flashcard = next_deck_flashcard
-    end 
-
-
-    previous_flashcards = @deck_flashcards.where('flashcard_id < ?', @deck_flashcard.flashcard_id)
-    @previous_flashcard = previous_flashcards.last || @flashcard
+    previous_deck_flashcards = @deck_flashcards.where('id < ?', @deck_flashcard.id)
+    previous_deck_flashcards.empty? ? @previous_deck_flashcard = nil : @previous_deck_flashcard = previous_deck_flashcards.last
   end 
 end
