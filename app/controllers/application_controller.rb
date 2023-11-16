88a6c9088ec7a1432f98f5d82e6c03d09111d0bc
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-11-16 09:04:01 +0100

    fixed the next and previous routing. issue was lack of understanding how to navigate through the collection of deck_flashcards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_first_deck_flashcard_id
+
+  private 
+
+  def set_first_deck_flashcard_id
+    if current_user
+      @first_deck_flashcard_id = current_user.deck.deck_flashcards.order(id: :asc).first.id
+    end
+  end 
 end
--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -1,6 +1,6 @@
 class StudiesController < ApplicationController
 
-  before_action :set_deck_and_flashcards, :set_flashcard_and_flashcard_statistics, :set_next_and_previous_flashcards
+  before_action :set_deck_and_deck_flashcards, :set_flashcard_and_flashcard_statistics, :set_next_and_previous_flashcards
 
   def show
   end
@@ -38,21 +38,38 @@ class StudiesController < ApplicationController
 
   private 
   
-  def set_deck_and_flashcards
+  def set_deck_and_deck_flashcards
     @deck = Deck.find_by(user_id: params[:user_id])
-    @flashcards = @deck.flashcards
+    puts "DECK BELOW"
+    puts @deck
+    @deck_flashcards = @deck.deck_flashcards.order(id: :asc)
+    puts "DECK_FLASHCARDS BELOW"
+    puts @deck_flashcards
+    @deck_flashcard = @deck_flashcards.find_by(id: params[:deck_flashcard]) || @deck_flashcards.find_by(id: params[:format])
+    puts "DECK_FLASHCARD BELOW"
+    puts @deck_flashcard
+    @flashcard = Flashcard.find(@deck_flashcard.flashcard_id) 
+    puts "FLASHCARD BELOW"
+    puts @flashcard 
   end 
 
   def set_flashcard_and_flashcard_statistics 
-    @flashcard = @flashcards.find(params[:id]) || @flashcards.first
-    @flashcard_statistics = @flashcard.flashcard_statistics.first
+    @flashcard_statistics = FlashcardStatistic.find_by(user_id: current_user, flashcard_id: @flashcard)
   end 
 
   def set_next_and_previous_flashcards
-    next_flashcards = @flashcards.where('flashcards.id > ?', @flashcard)
-    @next_flashcard = next_flashcards.first || @flashcard
+    next_deck_flashcards = @deck_flashcards.where('id > ?', @deck_flashcard.id)
+    if next_deck_flashcards.empty?
+      @next_deck_flashcard = nil
+    else  
+      next_deck_flashcard = next_deck_flashcards.first
 
-    previous_flashcards = @flashcards.where('flashcards.id < ?', @flashcard)
+      next_flashcard = Flashcard.find_by(id: next_deck_flashcard.flashcard_id) || Flashcard.find_by(id: @deck_flashcards.last.flashcard_id)
+      @next_deck_flashcard = next_deck_flashcard
+    end 
+
+
+    previous_flashcards = @deck_flashcards.where('flashcard_id < ?', @deck_flashcard.flashcard_id)
     @previous_flashcard = previous_flashcards.last || @flashcard
   end 
 end
