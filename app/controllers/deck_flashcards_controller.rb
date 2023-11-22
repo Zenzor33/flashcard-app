792b8d65df549344d19f28a340235a201110580d
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-11-22 10:29:14 +0100

    organised private methods in deckflashcards controller

--- app/controllers/deck_flashcards_controller.rb
+++ app/controllers/deck_flashcards_controller.rb
@@ -1,5 +1,5 @@
 class DeckFlashcardsController < ApplicationController
-  before_action :set_deck_and_deck_flashcards, :set_next_and_previous_flashcards
+  before_action :set_deck, :set_deck_flashcards, :set_deck_flashcard, :set_flashcard, :set_next_deck_flashcard, :set_previous_deck_flashcard 
 
   def show
   end
@@ -39,18 +39,30 @@ class DeckFlashcardsController < ApplicationController
 
   private 
   
-  def set_deck_and_deck_flashcards
+  def set_deck
     @deck = Deck.find_by(user_id: params[:user_id])
-    @deck_flashcards = @deck.deck_flashcards.order(id: :asc)
-    @deck_flashcard = @deck_flashcards.find_by(id: params[:id]) || @deck_flashcards.find_by(id: params[:format])
-
-    @flashcard = Flashcard.find(@deck_flashcard.flashcard_id) 
   end 
 
-  def set_next_and_previous_flashcards
+  def set_deck_flashcards 
+    @deck_flashcards = @deck.deck_flashcards.order(id: :asc)
+  end 
+
+  def set_deck_flashcard 
+    @deck_flashcard = @deck_flashcards.find_by(id: params[:id]) || @deck_flashcards.find_by(id: params[:format])
+  end 
+
+  def set_flashcard 
+    @flashcard = Flashcard.find(@deck_flashcard.flashcard_id) 
+  end
+
+  def set_next_deck_flashcard
     next_deck_flashcards = @deck_flashcards.where('id > ?', @deck_flashcard.id)
     next_deck_flashcards.empty? ? @next_deck_flashcard = nil : @next_deck_flashcard = next_deck_flashcards.first
+  end 
+
+  def set_previous_deck_flashcard
     previous_deck_flashcards = @deck_flashcards.where('id < ?', @deck_flashcard.id)
     previous_deck_flashcards.empty? ? @previous_deck_flashcard = nil : @previous_deck_flashcard = previous_deck_flashcards.last
   end 
+
 end
