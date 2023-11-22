748e9d1d7da1eb0e8bff1810718619d4a7eb607b
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-11-22 15:07:43 +0100

    refactor complete

--- app/controllers/deck_flashcards_controller.rb
+++ app/controllers/deck_flashcards_controller.rb
@@ -49,19 +49,20 @@ class DeckFlashcardsController < ApplicationController
   end 
 
   def set_deck_flashcard 
-    # Displays statistics and information in the study page (deck_flashcards#show) & decks stats_body (decks#show)
     @deck_flashcard = @deck_flashcards.find_by(id: params[:id]) 
   end 
 
   def set_next_deck_flashcard
     # Sets the link_to "next" in the study page (deck_flashcards#show)
     next_deck_flashcards = @deck_flashcards.where('id > ?', @deck_flashcard.id)
+    
     next_deck_flashcards.empty? ? @next_deck_flashcard = nil : @next_deck_flashcard = next_deck_flashcards.first
   end 
 
   def set_previous_deck_flashcard
     # Sets the link_to "previous" in the study page
     previous_deck_flashcards = @deck_flashcards.where('id < ?', @deck_flashcard.id)
+
     previous_deck_flashcards.empty? ? @previous_deck_flashcard = nil : @previous_deck_flashcard = previous_deck_flashcards.last
   end 
 
