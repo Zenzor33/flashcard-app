34c5d4921d5fcc31e0d13b17bbfcf5d2978ae8b9
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-11-26 13:12:00 +0100

    completed tests for flashcards_controller

--- test/controllers/flashcards_controller_test.rb
+++ test/controllers/flashcards_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class FlashcardsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  test "should get flashcard" do
+  test "should get flashcards_url" do
     #Create a user
     user = User.create!(email: 'test99999@example.com', password: 'password', password_confirmation: 'password')
     #Sign in the user
@@ -13,4 +13,29 @@ class FlashcardsControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
   end
+
+  #Should show flashcard page for @flashcard
+  test "should show flashcard" do 
+    #Create a user
+    user = User.create!(email: 'test99999@example.com', password: 'password', password_confirmation: 'password')
+    #Sign in the user
+    sign_in user 
+    #Make a request to flashcards#show
+    @flashcard = Flashcard.create!(front: "Hello World",back: "Ola Mundo")
+    get flashcard_path(@flashcard)
+
+    assert_response :success
+  end 
+
+  test "should redirect if flashcard not found" do 
+        #Create a user
+        user = User.create!(email: 'test99999@example.com', password: 'password', password_confirmation: 'password')
+        #Sign in the user
+        sign_in user 
+        #Make a request to flashcards#show
+        get flashcard_url(id: -1)
+
+        assert_redirected_to root_path
+        assert_equal 'Flashcard not found', flash[:alert]
+  end 
 end
