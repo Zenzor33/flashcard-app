527fce0863cb51f8af764e2cfa870ee1479af0f4
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-12-08 11:28:12 +0100

    updated flashcards requests spec

--- spec/requests/flashcards_spec.rb
+++ spec/requests/flashcards_spec.rb
@@ -1,7 +1,63 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Flashcards", type: :request do
-#   describe "GET /index" do
-#     pending "add some examples (or delete) #{__FILE__}"
-#   end
-# end
+RSpec.describe "Flashcards", type: :request do
+
+  before do
+    @user = FactoryBot.create(:user)
+    @flashcard = FactoryBot.create(:flashcard)
+    sign_in @user
+  end 
+
+  describe "GET /index" do
+
+    # before do
+    #   @user = FactoryBot.create(:user)
+    #   @flashcard = FactoryBot.create(:flashcard)
+    #   sign_in @user
+    # end 
+
+    it "returns HTTP success" do 
+      get flashcards_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do 
+
+    # before do
+    #   @user = FactoryBot.create(:user)
+    #   @flashcard = FactoryBot.create(:flashcard)
+    #   sign_in @user
+    # end 
+
+    context "With valid Flashcard ID" do 
+      it "returns HTTP success" do 
+        get flashcard_path(@flashcard)
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "With an invalid Flashcard ID" do 
+
+      before do
+        @user = FactoryBot.create(:user)
+        @flashcard = FactoryBot.create(:flashcard)
+        sign_in @user
+      end 
+
+      it "Returns 302 redirect" do 
+        get flashcard_path('invalid flashcard id')
+        expect(response).to have_http_status(302)
+      end 
+    end 
+  end   
+
+  context "When user is unauthenticated" do
+    before {sign_out @user}
+    it "redirects to sign_in page" do 
+      get flashcards_path 
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to "/users/sign_in"
+    end 
+  end
+end
