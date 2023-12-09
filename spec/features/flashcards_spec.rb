12f1dbb46aa7426c631aa1d75e260cd7901176d2
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-12-09 08:42:26 +0100

    DRY'd code in flashcards_spec feature

--- spec/features/flashcards_spec.rb
+++ spec/features/flashcards_spec.rb
@@ -1,45 +1,35 @@
 require 'rails_helper'
+# require_relative '../support/login_support'
 
 RSpec.feature "Flashcards", type: :feature do
-  scenario "New user signs in and adds a flashcard to their deck" do 
-    user = FactoryBot.create(:user)
-    
-    # Assuming flashcards are listed with an 'Add to Deck' button
-    first_flashcard = FactoryBot.create(:flashcard)
-    second_flashcard = FactoryBot.create(:flashcard)
-    third_flashcard = FactoryBot.create(:flashcard)
-    
-    visit new_user_session_path 
-    fill_in "Email", with: user.email 
-    fill_in "Password", with: user.password
-    click_button "Log in"
-    # visit flashcards_path 
 
+  let (:user ){ FactoryBot.create(:user) } #lazy loads user
+
+  before do 
+    FactoryBot.create_list(:flashcard, 3)
+    sign_in user
+  end 
+
+  scenario "New user signs in and adds a flashcard to their deck" do 
+
+    flashcard = Flashcard.first
+    visit flashcards_path
+    
     expect {
-      within("turbo-frame#flashcard_#{first_flashcard.id}") do
+      within("turbo-frame#flashcard_#{flashcard.id}") do
         find("form.button_to").click_button 'Add to Deck'
       end
     }.to change(user.deck.deck_flashcards, :count).by(1)
   end 
 
   scenario "New user signs in and removes a flashcard from their deck" do 
-    user = FactoryBot.create(:user)
-    
-    # Assuming flashcards are listed with an 'Add to Deck' button
-    first_flashcard = FactoryBot.create(:flashcard)
-    second_flashcard = FactoryBot.create(:flashcard)
-    third_flashcard = FactoryBot.create(:flashcard)
 
-    user.deck.add_flashcard(first_flashcard)
-
-    visit new_user_session_path 
-    fill_in "Email", with: user.email 
-    fill_in "Password", with: user.password
-    click_button "Log in"
-    # visit flashcards_path 
+    flashcard = Flashcard.first
+    user.deck.add_flashcard(flashcard)
+    visit flashcards_path
 
     expect {
-      within("turbo-frame#flashcard_#{first_flashcard.id}") do
+      within("turbo-frame#flashcard_#{flashcard.id}") do
         find("form.button_to").click_button 'Remove from Deck'
       end
     }.to change(user.deck.deck_flashcards, :count).by(-1)
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,5 +64,6 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 
 end
