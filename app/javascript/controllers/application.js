ef0f7624ae8b3aaa16b647035508e4ee7b069112
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2024-01-02 10:41:48 -0300

    for review

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,11 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
+const application = Application.start();
+import CheckboxSelectAll from "stimulus-checkbox-select-all";
+application.register("checkbox-select-all", CheckboxSelectAll);
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+export { application };
